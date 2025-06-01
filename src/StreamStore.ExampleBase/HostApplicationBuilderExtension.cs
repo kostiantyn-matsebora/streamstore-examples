e89cfaae1bde78fdc0d1e0b20f92196a1af6d988
kostiantyn-matsebora <kmatsebora@gmail.com>
2025-06-01 14:17:33 +0300

    Initial commit

---
+++ src/StreamStore.ExampleBase/HostApplicationBuilderExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.CommandLine;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using StreamStore.ExampleBase.Configuration;
+
+
+namespace StreamStore.ExampleBase
+{
+    [ExcludeFromCodeCoverage]
+    public static class HostApplicationBuilderExtension
+    {
+        public static RootCommand ConfigureExampleApplication(this HostApplicationBuilder builder, Action<ExampleApplicationConfigurator> configure)
+        {
+            builder.Configuration
+                    .AddJsonFile("appsettings.json", true)
+                    .AddJsonFile("appsettings.Development.json", true);
+
+            var appConfigurator = new ExampleApplicationConfigurator();
+            configure(appConfigurator);
+
+            return appConfigurator.ConfigureCommand(builder);
+        }
+    }
+}
