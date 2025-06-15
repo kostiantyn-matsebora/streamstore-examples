04dc69013ed0576bf610b91a6cebc8c17c5f5050
kostiantyn-matsebora <kmatsebora@gmail.com>
2025-06-15 03:04:36 +0300

    Add eventflow

---
+++ src/StreamStore.EventFlow.Example/AddNumberCommand.cs
@@ -0,0 +1,18 @@
+﻿using EventFlow.Aggregates.ExecutionResults;
+using EventFlow.Commands;
+
+namespace StreamStore.EventFlow.Example
+{
+	public class AddNumberCommand : Command<SumAggregate, SumId, IExecutionResult>
+	{
+		public AddNumberCommand(
+			SumId aggregateId,
+			int number)
+			: base(aggregateId)
+		{
+			Number = number;
+		}
+
+		public int Number { get; }
+	}
+}
---
+++ src/StreamStore.EventFlow.Example/AddNumberCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿
+using EventFlow.Aggregates.ExecutionResults;
+using EventFlow.Commands;
+
+namespace StreamStore.EventFlow.Example
+{
+	public class AddNumberCommandHandler : CommandHandler<SumAggregate, SumId, IExecutionResult, AddNumberCommand>
+	{
+		public override Task<IExecutionResult> ExecuteCommandAsync(
+			SumAggregate aggregate,
+			AddNumberCommand command,
+			CancellationToken cancellationToken)
+		{
+			var executionResult = aggregate.AddNumber(command.Number);
+			return Task.FromResult(executionResult);
+		}
+	}
+}
